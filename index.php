81d4c7b4d2cbbf1763a2d4686479d5fb8aab456f
David Diaz Montenegro <davidcol123dm@gmail.com>
2025-08-10 02:16:20 -0500

    cambios a subir

--- index.php
+++ index.php
@@ -10,10 +10,41 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     </head>
     <body>
         <?php
-        // put your code here
         echo "<h3>hola</h3>";
         echo "<h3>esto va en el segundo commit</h3>";
         echo "<h3>este es un cambio desde la rama de robledo</h3>";
+
+        // $comando = 'git log --pretty=format:"%h%d %s (%cd) - ENDLOG"';
+        $comando = 'git log --pretty=format:"%h %s DATEMAKE:(%cd) - MERGELOG%d ENDLOG"';
+        $salida = (string) shell_exec($comando);
+
+        $arrSalida = explode("ENDLOG", $salida);
+        foreach ($arrSalida as $key => $value) {
+            $initPostDate = strpos($value, "DATEMAKE");
+            $initMerge = strpos($value, "MERGELOG");
+            $commitId = substr($value,0,8);
+            $msg = substr($value, 8, $initPostDate - 8 );
+            $date = substr($value, $initPostDate + 9, 32 );
+            $merge = substr($value, $initMerge + 9, 100 );
+            $info = [
+                "commitId"=> $commitId,
+                "msg"=> $msg,
+                "date"=> $date,
+                "merge"=> $merge
+            ];
+            echo "<hr>";
+
+            // echo "<p>$value</p>";
+            echo $info['commitId'];
+            echo "<p></p>";
+            echo $info['msg'];
+            echo "<p></p>";
+            echo $info['date'];
+            echo "<p></p>";
+            echo $info['merge'];
+        }
+        echo "<hr>";
+
         ?>
     </body>
 </html>
