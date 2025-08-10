97c0406bd2f0772ca7134739f16e075331080dd3
David Diaz Montenegro <davidcol123dm@gmail.com>
2025-08-09 22:27:02 -0500

    este es un cambio desde la rama de david

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
         // put your code here
         echo "<h3>hola</h3>";
         echo "<h3>esto va en el segundo commit</h3>";
-        echo "<h3>este es un cambio desde la rama de david</h3>";
+        echo "<h3>este es un nuevo cambio desde la rama de david</h3>";
 
         ?>
     </body>
